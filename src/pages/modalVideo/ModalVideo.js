6e82eb4236a6db09788e733f0dce477025f90136
Oscar Gomez <ogomez510@gmail.com>
2021-10-28 22:39:59 +0200

    Create new movies

--- src/pages/modalVideo/ModalVideo.js
+++ src/pages/modalVideo/ModalVideo.js
@@ -3,7 +3,6 @@ import { Modal } from "antd";
 import ReactPlayer from "react-player";
 
 import "./ModalVideo.scss";
-import Menu from "rc-menu/lib/Menu";
 
 export default function ModalVideo(props) {
   const { videoKey, videoPlatform, isOpen, close } = props;
