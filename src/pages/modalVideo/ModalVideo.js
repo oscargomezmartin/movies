7f76ef9d42d804ebcbf902650efbee64908c3b49
Oscar Gomez Martin <oscargm@tempe.es>
2021-10-28 17:43:54 +0200

    Created Movie information

---
+++ src/pages/modalVideo/ModalVideo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import './ModalVideo.scss'
+
+export default function ModalVideo() {
+    return "Modal Video";
+}
--- src/pages/movie/movie.js
+++ src/pages/movie/movie.js
@@ -25,7 +25,7 @@ export default function Movie() {
 function RenderMovie(props) {
 
 const {
-  movieInfo: { title, backdrop_path } 
+  movieInfo: { title, backdrop_path , poster_path} 
 } = props;
 
 const backDropPath=`https://image.tmdb.org/t/p/original${backdrop_path}`;
@@ -38,11 +38,42 @@ const backDropPath=`https://image.tmdb.org/t/p/original${backdrop_path}`;
     <div className="movie__dark"></div>
       <Row>
         <Col span={8} offset={3} className="movie__poster">
-Caratula
+          <PosterMovie image={poster_path}/>
         </Col>
         <Col span={10} className="movie__info">
-          Movie Info
+          <MovieInfo movieInfo={props.movieInfo} />
         </Col>
       </Row>
   </div>);
+}
+
+function PosterMovie (props) {
+  const {image} = props;
+  const posterPath = `https://image.tmdb.org/t/p/original${image}`;
+
+  return <div style={{backgroundImage: `url(${posterPath})`}}/>
+}
+
+function MovieInfo(props) {
+  const {movieInfo: {id, title, release_date, overview, genres}} = props;
+
+  return <>
+    <div className="movie__info-header">
+      <h1>{title}
+        <span>{moment(release_date, "YYYY-MM-DD").format("YYYY")}</span>
+      </h1>
+      <button>
+        Ver trailer
+      </button>
+    </div>
+    <div className="movie__info-content">
+        <h3>General</h3>
+        <p>{overview}</p>
+
+        <h3>Géneros</h3>
+        <ul>{genres.map( genres => (
+          <li key={genres.id}> {genres.name} </li>
+        ))}</ul>
+      </div>
+  </>;
 }
